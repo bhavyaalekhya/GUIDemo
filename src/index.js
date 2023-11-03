07e7723b5564f92b5847c63574a48fa5e0b70cd4
Bhavya Alekhya <69102364+bhavyaalekhya@users.noreply.github.com>
2023-11-03 09:45:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,30 +3,54 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// Function to fetch and load JSON data
-// Function to fetch and load JSON data
-const fetchRecipes = async () => {
-  try {
-    // Import recipes.json directly from the src directory
-    const data = require('./recipes.json');
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
-  }
+// Set up a WebSocket connection
+const setupWebSocket = (setRecipes) => {
+  const socket = new WebSocket('ws://localhost:8000'); // Adjust the URL to match your server.py WebSocket address
+  
+    socket.onopen = () => {
+      console.log('WebSocket Connected');
+    };
+  
+    socket.onmessage = (event) => {
+      try {
+        // Assuming the server sends individual JSON recipe objects
+        const recipe = JSON.parse(event.data);
+        setRecipes((prevRecipes) => [...prevRecipes, recipe]);
+      } catch (error) {
+        console.error('Error parsing data:', error);
+      }
+    };
+  
+    // ...
+  
+    return socket; // Return socket if you need to send messages to the server
+  };
+  
+
+const Main = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    // Initialize WebSocket connection and handle incoming data
+    const socket = setupWebSocket(setRecipes);
+
+    // Clean up the WebSocket connection when this component is unmounted
+    return () => {
+      if (socket) {
+        socket.close();
+      }
+    };
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <App recipes={recipes} />
+    </React.StrictMode>
+  );
 };
 
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
 
-// Render the app with fetched recipes
-fetchRecipes()
-  .then((recipes) => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <App recipes={recipes} />
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
-  })
-  .catch((error) => {
-    console.error('Error rendering app:', error);
-  });
