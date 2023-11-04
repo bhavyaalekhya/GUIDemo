4d333cb00019405ee93ebf2b46ad17f2eeecae4a
Bhavya Alekhya <69102364+bhavyaalekhya@users.noreply.github.com>
2023-11-03 22:59:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import AlternateReverseTimeline from './AlternateReverseTimeline';
 
@@ -10,13 +10,79 @@ function App({ recipes }) {
     height: '600px',
   };
 
+  console.log("Recipes: ------------------------------",recipes);
+
+  const UPDATE_RECIPE = "update recipe";
+  const UPDATE_STATUS = "update status";
+  const UPDATE_ERRORS = "update errors";
+
+  const initializeRecipeMap = (recipes) => {
+    let map = new Map();
+    recipes.forEach((recipe) => {
+      console.log("Type:",typeof(recipe.Details.RecipeState));
+      map.set(recipe.Details.Recipe, {
+        Recipe: recipe.Details.Recipe,
+        RecipeState: recipe.Details.RecipeState, // Assuming RecipeState is a JSON string
+        Errors: recipe.Details.Errors || []
+      });
+    });
+    console.log("Map Value:",map);
+    return map;
+  };
+
+  initializeRecipeMap(recipes);
+
+  // Initialize recipeMap state with recipes prop
+  const [recipeMap, setRecipeMap] = useState(() => initializeRecipeMap(recipes));
+
+  useEffect(() => {
+    recipes.forEach((recipe) => {
+      console.log(recipe);
+      if (recipe.Type === UPDATE_RECIPE) {
+        const existingRecipe = recipeMap.get(recipe.Details.Recipe);
+        if (existingRecipe) {
+          // Update the existing recipe
+          existingRecipe.RecipeState = recipe.Details.RecipeState;
+        } else {
+          // Add a new recipe to the map
+          const newRecipe = {
+            Recipe: recipe.Details.Recipe,
+            RecipeState: recipe.Details.RecipeState,
+          };
+          setRecipeMap((prevRecipeMap) => new Map(prevRecipeMap.set(recipe.Details.Recipe, newRecipe)));
+        }
+      } else if (recipe.Type === UPDATE_STATUS) {
+        const existingRecipe = recipeMap.get(recipe.Details.Recipe);
+        if (existingRecipe) {
+          existingRecipe.RecipeState = recipe.Details.RecipeState;
+        } else {
+          const newRecipe = {
+            Recipe: recipe.Details.Recipe,
+            RecipeState: recipe.Details.RecipeState,
+          };
+          setRecipeMap((prevRecipeMap) => new Map(prevRecipeMap.set(recipe.Details.Recipe, newRecipe)));
+        }
+      } else if (recipe.Type === UPDATE_ERRORS) {
+        const existingRecipe = recipeMap.get(recipe.Details.Recipe);
+        if (existingRecipe) {
+          existingRecipe.Errors = recipe.Details.Errors;
+        } else {
+          const newRecipe = {
+            Recipe: recipe.Details.Recipe,
+            RecipeState: [],
+            Errors: recipe.Details.Errors,
+          };
+          setRecipeMap((prevRecipeMap) => new Map(prevRecipeMap.set(recipe.Details.Recipe, newRecipe)));
+        }
+      }
+    });
+  }, [recipes, recipeMap]);
+
   return (
     <div className="App">
-      {recipes.map((recipe) => (
-        // Use a unique identifier for each recipe as the key
-        <div key={recipe.Steps_ID} className="segment" style={segmentStyle}>
-          {/* Pass the SubSteps array to the component */}
-          <AlternateReverseTimeline steps={recipe.Steps} />
+      {[...recipeMap.values()].map((recipe) => (
+        <div key={recipe.Recipe} className="segment" style={segmentStyle}>
+          <AlternateReverseTimeline steps={recipe.RecipeState} />
         </div>
       ))}
     </div>
